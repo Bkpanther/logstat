c83c085bf5e55dea7fdc858d476366eeca54a0bd
AlexanderKaraberov <alexander@spotme.com>
2020-02-16 21:04:25 +0100

    fix(log parser): fix out_of_range exception, switch to operator [] with manual size checking instead of a more expensive at()

--- src/couchdb_log_parser.cc
+++ src/couchdb_log_parser.cc
@@ -7,37 +7,36 @@
 namespace log_parsers {
 
 Log CouchDBLogParser::ParseLog(const std::vector<std::string> &tokens) const { 
-    if (tokens.size() > 1) {
-        const auto level = log_utils::GetLogLevel(tokens.at(0));
-        const std::time_t log_date = log_utils::GetEpoch(tokens.at(1), log_parsers::kCouchdbLogDateFormat);
-        if (level != log_utils::LogSeverityLevel::UNDEF && log_date != -1) {
-            const std::string host = tokens.at(2);
-            const std::string pid = tokens.at(3);
-            const auto method = log_utils::GetHTTPMethod(tokens.at(8));
-            if (tokens.at(4).compare(log_parsers::kCouchdbNonReqMarker) == 0) {
-                Log couchdb_log = {level, log_date, host, pid};
-                couchdb_log.is_generic_msg = true;
-                return couchdb_log;
-            } 
-            else if (method != log_utils::HTTPMethod::UNDEF_ERROR) {
-                const std::string msg_id = tokens.at(4);
-                const std::string domain_name = tokens.at(5);
-                const std::string ip_address = tokens.at(6);
-                const std::string user = tokens.at(7);
-                const std::string request_url = tokens.at(9);
-                const std::string http_code = tokens.at(10);
-                const int req_time = std::stoi(tokens.at(12), nullptr);
-                Log couchdb_log = {level, log_date, host, pid, msg_id, domain_name, ip_address, user,
-                                         method, request_url, http_code, req_time};
-                couchdb_log.short_req_url = request_url.substr(0, request_url.find('?'));                         
-                couchdb_log.http_method_str = tokens.at(8);                       
-                return couchdb_log;
-            }
-            else {
-                Log couchdb_log = {level, log_date, host, pid};
-                couchdb_log.is_stacktrace = true;
-                return couchdb_log;
-            }
+    const auto size = tokens.size();
+    const auto level = size > 0 ? log_utils::GetLogLevel(tokens[0]) : log_utils::LogSeverityLevel::UNDEF;
+    const std::time_t log_date = size > 1 ? log_utils::GetEpoch(tokens[1], log_parsers::kCouchdbLogDateFormat) : -1;
+    if (level != log_utils::LogSeverityLevel::UNDEF && log_date != -1) {
+        const std::string host = size > 2 ? tokens[2] : "";
+        const std::string pid = size > 3 ? tokens[3] : "";
+        const auto method = size > 8 ? log_utils::GetHTTPMethod(tokens[8]) : log_utils::HTTPMethod::UNDEF_ERROR;
+        const std::string msg_id = size > 4 ? tokens[4] : log_parsers::kCouchdbNonReqMarker;
+        if (msg_id.compare(log_parsers::kCouchdbNonReqMarker) == 0) {
+            Log couchdb_log = {level, log_date, host, pid};
+            couchdb_log.is_generic_msg = true;
+            return couchdb_log;
+        }
+        else if (method != log_utils::HTTPMethod::UNDEF_ERROR) {
+            const std::string domain_name = size > 5 ? tokens[5] : "";
+            const std::string ip_address = size > 6 ? tokens[6] : "";
+            const std::string user = size > 7 ? tokens[7] : "";
+            const std::string request_url = size > 9 ? tokens[9] : "";
+            const std::string http_code = size > 10 ? tokens[10] : "";
+            const int req_time = std::stoi(size > 12 ? tokens[12] : "0", nullptr);
+            Log couchdb_log = {level, log_date, host, pid, msg_id, domain_name, ip_address, user,
+                                        method, request_url, http_code, req_time};
+            couchdb_log.short_req_url = request_url.substr(0, request_url.find('?'));                         
+            couchdb_log.http_method_str = size > 8 ? tokens[8] : "";                       
+            return couchdb_log;
+        }
+        else {
+            Log couchdb_log = {level, log_date, host, pid};
+            couchdb_log.is_stacktrace = true;
+            return couchdb_log;
         }
     }
     return {};
