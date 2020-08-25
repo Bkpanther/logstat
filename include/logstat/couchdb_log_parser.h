754cf5ac3dd73cd55e2c8f3daf12fe2196eb4d51
TARUN <ticolisp@gmail.com>
2020-08-25 15:08:45 +0530

    Update couchdb_log_parser.h

--- include/logstat/couchdb_log_parser.h
+++ include/logstat/couchdb_log_parser.h
@@ -1,9 +1,4 @@
-// Copyright (c) 2019 Oleksandr Karaberov. All rights reserved.
-// Use of this source code is governed by a MIT-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
-
 #include "log_parser.h"
-
 namespace logstat {
 
 static char const *kCouchdbLogDateFormat = "%Y-%m-%dT%H:%M:%S";
@@ -13,4 +8,4 @@ class CouchDBLogParser : public LogParser {
  public:
   Log ParseLog(const std::vector<std::string> &tokens) const override;
 };
-}  // namespace logstat
+}
