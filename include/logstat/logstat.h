2c9c262ec29b3c738534eb5905cd6294095d5d32
TARUN <ticolisp@gmail.com>
2020-08-25 14:52:59 +0530

    Update logstat.h

--- include/logstat/logstat.h
+++ include/logstat/logstat.h
@@ -1,7 +1,3 @@
-// Copyright (c) 2019 Oleksandr Karaberov. All rights reserved.
-// Use of this source code is governed by a MIT-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
-
 #include <algorithm>
 #include <functional>
 #include <unordered_map>
@@ -13,17 +9,12 @@ class LogParser;
 
 namespace logstat {
 
-Log FetchDateByOffset(const std::ifstream &infile, const std::streampos offset,
-                      const LogParser &parser) noexcept;
+Log FetchDateByOffset(const std::ifstream &infile, const std::streampos offset, const LogParser &parser) noexcept;
 
-std::streampos FindLogPos(std::ifstream &infile, const time_t target,
-                          const size_t file_size,
-                          const LogParser &parser) noexcept;
+std::streampos FindLogPos(std::ifstream &infile, const time_t target, const size_t file_size, const LogParser &parser) noexcept;
 
-std::streampos AdjustLogPos(const std::ifstream &infile, const time_t start,
-                            std::streampos offset,
-                            const LogParser &parser) noexcept;
+std::streampos AdjustLogPos(const std::ifstream &infile, const time_t start, std::streampos offset, const LogParser &parser) noexcept;
 
 void ProcessLogs(const CmdOptions &options, const LogParser &parser) noexcept;
 
-}  // namespace logstat
+}
